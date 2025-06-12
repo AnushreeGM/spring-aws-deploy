f44da4dbc11a7626a44e72a075337434cedd877f
AnushreeGM <117091147+AnushreeGM@users.noreply.github.com>
2025-06-12 11:46:05 +0530

    Update TestController.java

--- src/main/java/com/example/spring_aws_deploy/TestController.java
+++ src/main/java/com/example/spring_aws_deploy/TestController.java
@@ -1,4 +1,4 @@
-package com.example.springbootawsdeploy;
+package com.example.spring_aws_deploy;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
